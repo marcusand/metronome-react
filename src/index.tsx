770461e44befac0a88d6b238bb83142da95d49c1
Marcus Schreiter <mail@marcusand.de>
2022-09-09 14:37:53 +0200

    add redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import { App } from './components/App/App';
 import { store } from './state/store';
 import './index.scss';
 import { initializeMetronome } from './state/slices/metronome';
+import { Provider } from 'react-redux';
 
 store.dispatch(initializeMetronome());
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
