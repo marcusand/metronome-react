d18c7e47fc0f8d86c9ccf7b16bae4e786bbf7468
Marcus Schreiter <mail@marcusand.de>
2022-09-09 13:23:21 +0200

    add react router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 interface Props {}
 
 export const App: React.FC<Props> = () => {
-  return <div>App</div>;
+  return (
+    <div className="outer-container">
+      <div className="inner-container">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div>home</div>} />
+            <Route path="/about" element={<div>about</div>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
 };
