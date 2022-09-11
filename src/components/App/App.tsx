f8be7cd9a72acbf6cc84ef64ffed34435ca98707
Marcus Schreiter <mail@marcusand.de>
2022-09-11 17:38:44 +0200

    handle pending and failed state

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { selectLoading } from '../../state/selectors/metronome';
 import { About } from '../Routes/About/About';
 import { Home } from '../Routes/Home/Home';
 import './App.scss';
@@ -7,15 +9,21 @@ import './App.scss';
 interface Props {}
 
 export const App: React.FC<Props> = () => {
+  const loading = useSelector(selectLoading);
+
   return (
     <div className="outer-container">
       <div className="inner-container">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </BrowserRouter>
+        {loading === 'pending' && <span>loading...</span>}
+        {loading === 'failed' && <span>error loading samples :/</span>}
+        {loading === 'succeeded' && (
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </BrowserRouter>
+        )}
       </div>
     </div>
   );
--- src/state/selectors/metronome.ts
+++ src/state/selectors/metronome.ts
@@ -1,5 +1,6 @@
 import { RootState } from '../store';
 
+export const selectLoading = (state: RootState) => state.metronome.loading;
 export const selectBpm = (state: RootState) => state.metronome.bpm;
 export const selectTimeSignature = (state: RootState) => state.metronome.timeSignature;
 export const selectSampleSet = (state: RootState) => state.metronome.sampleSet;
