67eeca95e7613f6d61dd7244447f252c048552f5
Marcus Schreiter <mail@marcusand.de>
2022-09-11 17:57:45 +0200

    implement about page

--- src/components/Routes/About/About.tsx
+++ src/components/Routes/About/About.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './About.scss';
 
-interface Props {}
-
-export const About: React.FC<Props> = () => {
-  return <div>About</div>;
+export const About: React.FC = () => {
+  return (
+    <div className="about">
+      <section>
+        <h2>offline usage & installation</h2>
+        <p>
+          This metronome is offline capable and installable. This means on ios or android you can
+          add it to your homescreen and use it like a native app or you can "install" it on your
+          desktop computer.
+        </p>
+        <p>
+          For more information see{' '}
+          <a
+            href="https://mobilesyrup.com/2020/05/24/how-install-progressive-web-app-pwa-android-ios-pc-mac/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            this guide
+          </a>
+          .
+        </p>
+      </section>
+      <section>
+        <h2>credits</h2>
+        <p>
+          By{' '}
+          <a href="https://github.com/marcusand" target="__blank">
+            marcusand
+          </a>
+          . Report any issues on <a href="https://github.com/marcusand/metronome">github</a> or send
+          me an <a href="mailto:mail@marcusand.de">email</a>
+        </p>
+      </section>
+      <div className="back">
+        <Link to="/">back</Link>
+      </div>
+    </div>
+  );
 };
