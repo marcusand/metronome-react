3af85c10b39c3e93e2367894c1681ec5fc6b2edd
Marcus Schreiter <mail@marcusand.de>
2022-09-11 12:14:38 +0200

    remove RowContainer component and use grid layout instead

--- src/components/Metronome/Metronome.tsx
+++ src/components/Metronome/Metronome.tsx
@@ -8,7 +8,6 @@ import {
   selectVolume,
 } from '../../state/selectors/metronome';
 import { BpmInfo } from './BpmInfo/BpmInfo';
-import { RowContainer } from './RowContainer/RowContainer';
 import { TapButton } from './TapButton/TapButton';
 import { Slider } from './Slider/Slider';
 import { Poti } from './Poti/Poti';
@@ -37,29 +36,19 @@ export const Metronome: React.FC<Props> = () => {
   const setVolume = (value: number) => dispatch(metronome.setVolume(value));
 
   return (
-    <>
-      <RowContainer>
-        <BpmInfo>{bpm}</BpmInfo>
-      </RowContainer>
-      <RowContainer>
-        <TapButton minBpm={MIN_BPM} maxBpm={MAX_BPM} onTap={setBpm} />
-      </RowContainer>
-      <RowContainer width="100%">
-        <Slider id="bpm-slider" min={MIN_BPM} max={MAX_BPM} value={bpm} onChange={setBpm} />
-      </RowContainer>
-      <RowContainer width="60%">
-        <div className="buttons">
-          <Poti title="time" stepsCount={12} value={timeSignature} onChange={setTimeSignature} />
-          <PlayButton playing={playing} onPlay={setPlay} onPause={setPause} />
-          <Poti title="sound" stepsCount={3} value={sampleSet} onChange={setSampleSet} />
-        </div>
-      </RowContainer>
-      <RowContainer width="60%">
-        <div className="volume-slider">
-          <Slider id="volume-slider" min={0} max={100} value={volume} onChange={setVolume} />
-          <label htmlFor="volume-slider">volume</label>
-        </div>
-      </RowContainer>
-    </>
+    <div className="metronome">
+      <BpmInfo>{bpm}</BpmInfo>
+      <TapButton minBpm={MIN_BPM} maxBpm={MAX_BPM} onTap={setBpm} />
+      <Slider id="bpm-slider" min={MIN_BPM} max={MAX_BPM} value={bpm} onChange={setBpm} />
+      <div className="buttons">
+        <Poti title="time" stepsCount={12} value={timeSignature} onChange={setTimeSignature} />
+        <PlayButton playing={playing} onPlay={setPlay} onPause={setPause} />
+        <Poti title="sound" stepsCount={3} value={sampleSet} onChange={setSampleSet} />
+      </div>
+      <div className="volume-slider">
+        <Slider id="volume-slider" min={0} max={100} value={volume} onChange={setVolume} />
+        <label htmlFor="volume-slider">volume</label>
+      </div>
+    </div>
   );
 };
