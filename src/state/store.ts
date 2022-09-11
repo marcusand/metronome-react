910b190f1d5424b20fb62a1aa4ebdf157997f692
Marcus Schreiter <mail@marcusand.de>
2022-09-11 16:18:11 +0200

    persist redux store with local storage

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,11 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { reducer as metronomeReducers } from './slices/metronome';
+import { loadFromLocalStorage, saveToLocalStorage } from './localStorage';
+import { MetronomeState, reducer as metronomeReducers } from './slices/metronome';
+
+const localStorageItemName = 'metronome-state';
+
+export type RootState = {
+  metronome: MetronomeState;
+};
 
 export const store = configureStore({
   reducer: {
     metronome: metronomeReducers,
   },
+  preloadedState: loadFromLocalStorage<RootState>(localStorageItemName),
 });
 
-export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+store.subscribe(() => saveToLocalStorage<RootState>(store.getState(), localStorageItemName));
