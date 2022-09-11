4db6cf550b74317e4fd3d2ea501aca3e1683a9c9
Marcus Schreiter <mail@marcusand.de>
2022-09-11 11:21:41 +0200

    add volume slider

--- src/components/Metronome/Metronome.tsx
+++ src/components/Metronome/Metronome.tsx
@@ -5,6 +5,7 @@ import {
   selectPlaying,
   selectSampleSet,
   selectTimeSignature,
+  selectVolume,
 } from '../../state/selectors/metronome';
 import { BpmInfo } from './BpmInfo/BpmInfo';
 import { RowContainer } from './RowContainer/RowContainer';
@@ -26,12 +27,14 @@ export const Metronome: React.FC<Props> = () => {
   const timeSignature = useSelector(selectTimeSignature);
   const sampleSet = useSelector(selectSampleSet);
   const playing = useSelector(selectPlaying);
+  const volume = useSelector(selectVolume);
 
   const setBpm = (value: number) => dispatch(metronome.setBpm(value));
   const setTimeSignature = (value: number) => dispatch(metronome.setTimeSignature(value));
   const setSampleSet = (value: number) => dispatch(metronome.setSampleSet(value));
   const setPlay = () => dispatch(metronome.play());
   const setPause = () => dispatch(metronome.pause());
+  const setVolume = (value: number) => dispatch(metronome.setVolume(value));
 
   return (
     <>
@@ -49,6 +52,12 @@ export const Metronome: React.FC<Props> = () => {
           <Poti title="time" stepsCount={12} value={timeSignature} onChange={setTimeSignature} />
           <PlayButton playing={playing} onPlay={setPlay} onPause={setPause} />
           <Poti title="sound" stepsCount={3} value={sampleSet} onChange={setSampleSet} />
+        </div>
+      </RowContainer>
+      <RowContainer width="60%">
+        <div className="volume-slider">
+          <Slider id="volume-slider" min={0} max={100} value={volume} onChange={setVolume} />
+          <label htmlFor="volume-slider">volume</label>
         </div>
       </RowContainer>
     </>
--- src/state/selectors/metronome.ts
+++ src/state/selectors/metronome.ts
@@ -4,3 +4,4 @@ export const selectBpm = (state: RootState) => state.metronome.bpm;
 export const selectTimeSignature = (state: RootState) => state.metronome.timeSignature;
 export const selectSampleSet = (state: RootState) => state.metronome.sampleSet;
 export const selectPlaying = (state: RootState) => state.metronome.playing;
+export const selectVolume = (state: RootState) => state.metronome.volume;
